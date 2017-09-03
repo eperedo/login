4345bc414688d9dfa6a7a156e36051ab07797bfb
Eduardo Peredo <jrperedo@gmail.com>
2017-09-03 17:44:25 -0500

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+	<div id="app">
+		<login></login>
+	</div>
+</template>
+
+<script>
+import Login from './components/Login';
+
+export default {
+	name: 'app',
+	components: {
+		Login,
+	},
+};
+</script>
+
+<style lang="scss">
+html {
+	font-size: 16px;
+}
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,23 @@
+<template>
+	<section class="login-container">
+
+	</section>
+</template>
+
+<script>
+export default {
+	name: 'Login',
+};
+</script>
+
+<style lang="scss" scoped>
+.login-container {
+	background-image: radial-gradient(50% 84%, map-get($colors, green) 13%, map-get($colors, dark-green) 87%);
+	display: flex;
+	height: 800px;
+	margin: auto;
+	width: 800px;
+}
+</style>
+
+
